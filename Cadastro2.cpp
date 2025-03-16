36a3fd30bc1fc3648fcd0d349126e927eaccc9ea
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-15 23:29:34 -0300

    Feat: Structs

--- Cadastro2.cpp
+++ Cadastro2.cpp
@@ -7,7 +7,36 @@
 
 using namespace std;
 
+struct Associado
+{
+    int codigo, num_dependentes;
+    string cpf, data_associacao, tipo_socio, nome, data_nasc, endereco, sexo, telefone, email;
+    double mensalidade;
+};
+
+struct Dependente
+{
+    int codigo, codExterno;
+    string cpf_associado, nome, data_nasc, sexo, telefone, email;
+};
+
+struct Visitante
+{
+    int codigo, num_visitas;
+    string cpf, codigo_associado, nome, data_nasc, sexo, data_visita_inicial, data_visita_final;
+};
+
 int main(){
+    setlocale(LC_ALL, "Portuguese");
+    system("cls");
+
+    int prox_codigo = 0;
+    int prox_codigode = 0;
+    int prox_codigovi = 0;
+
+    vector<Associado> associados;
+    vector<Dependente> dependentes;
+    vector<Visitante> visitantes;
 
     return 0;
 }
